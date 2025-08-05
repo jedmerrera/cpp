88e3123a01119bbb3624b778c37de1d195b5501e
jedchr <jedmerrera.0203@gmail.com>
2025-08-05 12:16:50 +0800

    Add files via upload

---
+++ simple information.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    string name = "Jed";
+    int age = 17;
+    double height = 160.23;
+    float weight = 43.10;
+    char gender = 'M';
+    bool student = true;
+
+    cout << name << endl;
+    cout << age << endl;
+    cout << height << endl;
+    cout << weight << endl;
+    cout << gender  << endl;
+    cout << student << endl;
+        
+
+    return 0;
+}
