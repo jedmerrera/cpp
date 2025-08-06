614848851c796a7ebd75af220a66644e78a2a174
jedchr <jedmerrera.0203@gmail.com>
2025-08-06 23:52:15 +0800

    Add files via upload

---
+++ comparison operators_equal to.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    
+    int a = 7;
+    int b = 5;
+
+    int equalTo = a == b;
+
+    cout << equalTo << endl;
+
+    return 0;
+}
+
+
+// Comparison operators / true - 1, false - 0;
+// equal to - ==
+// not equal to - !=
+// greater than - >
+// less than - <
+// greater than or equal to - >=
+// less than or equal to - <=
