4bffffbc825fd8c8a34a4c084515b7766c5ca986
jedchr <jedmerrera.0203@gmail.com>
2025-08-13 12:56:56 +0800

    Add files via upload

---
+++ outer_inner_loop.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // outer loop
+
+   for (int i = 1; i <= 3; ++i){
+    cout << "Test: " << i << endl;
+
+    // inner loop
+
+    for (int j = 1; j <= 5; ++j){
+    cout << j << endl;
+
+    }
+
+   }
+
+
+    return 0;
+}
+
